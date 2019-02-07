7eb4fa914778bf1400b43acd238d69764b3a9fde
Michael Rawson <michael@rawsons.uk>
2019-02-07 11:32:21 +0000

    fix resolve_include

--- src/resolve.rs
+++ src/resolve.rs
@@ -3,6 +3,8 @@ use std::fs::File;
 use std::io;
 use std::path::{Path, PathBuf};
 
+use crate::syntax::Included;
+
 fn open(path: &Path) -> Result<File, io::Error> {
     let path = path.canonicalize()?;
     File::open(path)
@@ -17,7 +19,7 @@ fn open_tptp(path: &Path) -> Result<File, io::Error> {
 /// Resolve a TPTP include directive to a `File`.
 ///
 /// Files are opened read-only and relative to the current directory, or if that fails, relative to the `TPTP` environment variable.
-pub fn resolve_include(input: &str) -> Result<File, io::Error> {
-    let path = Path::new(input);
+pub fn resolve_include(included: Included) -> Result<File, io::Error> {
+    let path = PathBuf::from(included);
     open(&path).or_else(|_| open_tptp(&path))
 }
